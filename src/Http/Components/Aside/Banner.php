eeca4df86739a26eece25ef37e36ae04d26b480d
Carlos Tamagawa <carlos@mixd.com.br>
2025-04-23 08:30:26 -0300

    fix(menu) ajusta o visible do menu

--- src/Http/Components/Aside/Banner.php
+++ src/Http/Components/Aside/Banner.php
@@ -4,6 +4,8 @@ namespace Agenciafmd\Banners\Http\Components\Aside;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Illuminate\Support\Facades\Gate;
+use Agenciafmd\Banners\Models\Banner as BannerModel;
 
 class Banner extends Component
 {
@@ -23,7 +25,7 @@ class Banner extends Component
         $this->label = __(config('admix-banners.name'));
         $this->url = route('admix.banners.index');
         $this->active = request()?->currentRouteNameStartsWith('admix.banners');
-        $this->visible = true;
+        $this->visible = Gate::allows('view', BannerModel::class);
 
         return view('admix::components.aside.item');
     }
